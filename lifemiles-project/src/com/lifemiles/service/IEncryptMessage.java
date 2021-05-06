1184b3f243b7b75b1ddbdc9a104b843f552ae74a
Rodolfo <rodolfomolinasv@gmail.com>
2021-05-05 20:27:26 -0600

    Add EncryptMessage Interface

---
+++ lifemiles-project/src/com/lifemiles/service/IEncryptMessage.java
@@ -0,0 +1,11 @@
+package com.lifemiles.service;
+
+public interface IEncryptMessage {
+
+	public String medicalUnit(String text);
+
+	public String infantryUnit(String text);
+
+	public String airUnit(String text);
+
+}
